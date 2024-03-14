6883c6e5c640044c590470aac6ac8d1378aff4d0
Pol Bosch <pol.hcsob@gmail.com>
2024-03-14 12:10:26 +0100

    JS String Exercises

--- Aspasia/JavaScript/13-03-24/StringExercises.js
+++
@@ -1,6 +0,0 @@
-// Escribe una función llamada longitud que reciba como parámetro de entrada una cadena de texto y devuelva el número de letras que tiene.
-
-// Por ejemplo la siguiente llamada a la función:
-
-const length = (str) => str.length();
-length("Ana");
