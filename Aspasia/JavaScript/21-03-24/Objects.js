c96dc0614e42bdb947f5d55d80709ba2a2a664c7
Pol Bosch <pol.hcsob@gmail.com>
2024-03-21 13:56:03 +0100

    Object.find js

--- Aspasia/JavaScript/21-03-24/Objects.js
+++ Aspasia/JavaScript/21-03-24/Objects.js
@@ -137,3 +137,42 @@ const myPlants = [
 
 const secondTree = myPlants[1].list;
 console.log(secondTree); // Debería mostrar: ["fir", "pine", "birch"]
+
+//Utilizando el método find, busca el usuario cuyo nombre es Juan y muestra su edad.
+
+const users = [
+    {
+        "name": "Paco",
+        "isActive": false,
+        "age": 18
+    },
+    {
+        "name": "Laura",
+        "isActive": false,
+        "age": 21
+    },
+    {
+        "name": "Raquel",
+        "isActive": false,
+        "age": 15
+    },
+    {
+        "name": "Juan",
+        "isActive": true,
+        "age": 17
+    },
+    {
+        "name": "Alberto",
+        "isActive": false,
+        "age": 50
+    },
+    {
+        "name": "Rodolfo",
+        "isActive": true,
+        "age": 7
+    },
+];
+
+const choosedUser = users.find(user => user.name === "Juan").age; // Modifica esta línea para resolver el ejercicio
+
+console.log(choosedUser); // Debería mostrar: 17
